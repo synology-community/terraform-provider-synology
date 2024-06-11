299f9189b253408ca05e1d2984f7d404168e1188
appkins <nbatkins@gmail.com>
2024-06-10 21:25:45 -0500

    Update projects

--- synology/provider/container/models/service.go
+++ synology/provider/container/models/service.go
@@ -22,12 +22,34 @@ type Image struct {
 	Tag        types.String `tfsdk:"tag"`
 }
 
+type ServiceNetwork struct {
+	Name         types.String `tfsdk:"name"`
+	Aliases      types.Set    `tfsdk:"aliases"`
+	Ipv4Address  types.String `tfsdk:"ipv4_address"`
+	Ipv6Address  types.String `tfsdk:"ipv6_address"`
+	LinkLocalIPs types.Set    `tfsdk:"link_local_ips"`
+	MacAddress   types.String `tfsdk:"mac_address"`
+	DriverOpts   types.Map    `tfsdk:"driver_opts"`
+	Priority     types.Int64  `tfsdk:"priority"`
+}
+
+type Port struct {
+	Name        types.String `tfsdk:"name"`
+	Target      types.Int64  `tfsdk:"target"`
+	Published   types.String `tfsdk:"published"`
+	Protocol    types.String `tfsdk:"protocol"`
+	AppProtocol types.String `tfsdk:"app_protocol"`
+	Mode        types.String `tfsdk:"mode"`
+	HostIP      types.String `tfsdk:"host_ip"`
+}
+
 type Service struct {
 	Name     types.String `tfsdk:"name"`
 	Image    types.Set    `tfsdk:"image"`
 	Replicas types.Int64  `tfsdk:"replicas"`
 	Logging  types.Set    `tfsdk:"logging"`
-	// Ports    types.SetType `tfsdk:"ports"`
+	Ports    types.Set    `tfsdk:"port"`
+	Networks types.Set    `tfsdk:"network"`
 }
 
 func (m Service) ModelType() attr.Type {
@@ -47,6 +69,33 @@ func (m Service) AttrType() map[string]attr.Type {
 			},
 		},
 		"replicas": types.Int64Type,
+		"port": types.SetType{
+			ElemType: types.ObjectType{
+				AttrTypes: map[string]attr.Type{
+					"name":         types.StringType,
+					"target":       types.Int64Type,
+					"published":    types.StringType,
+					"protocol":     types.StringType,
+					"app_protocol": types.StringType,
+					"mode":         types.StringType,
+					"host_ip":      types.StringType,
+				},
+			},
+		},
+		"network": types.SetType{
+			ElemType: types.ObjectType{
+				AttrTypes: map[string]attr.Type{
+					"name":           types.StringType,
+					"aliases":        types.SetType{ElemType: types.StringType},
+					"ipv4_address":   types.StringType,
+					"ipv6_address":   types.StringType,
+					"link_local_ips": types.SetType{ElemType: types.StringType},
+					"mac_address":    types.StringType,
+					"driver_opts":    types.MapType{ElemType: types.StringType},
+					"priority":       types.Int64Type,
+				},
+			},
+		},
 		"logging": types.SetType{
 			ElemType: types.ObjectType{
 				AttrTypes: map[string]attr.Type{
@@ -64,6 +113,8 @@ func (m Service) Value() attr.Value {
 
 	var logging basetypes.SetValue
 	var image basetypes.SetValue
+	var ports basetypes.SetValue
+	var networks basetypes.SetValue
 
 	if l, diag := m.Logging.ToSetValue(context.Background()); !diag.HasError() {
 		logging = l
@@ -73,10 +124,20 @@ func (m Service) Value() attr.Value {
 		image = i
 	}
 
+	if p, diag := m.Ports.ToSetValue(context.Background()); !diag.HasError() {
+		ports = p
+	}
+
+	if n, diag := m.Networks.ToSetValue(context.Background()); !diag.HasError() {
+		networks = n
+	}
+
 	return types.ObjectValueMust(m.AttrType(), map[string]attr.Value{
 		"name":     types.StringValue(m.Name.ValueString()),
 		"image":    image,
 		"replicas": types.Int64Value(m.Replicas.ValueInt64()),
+		"port":     ports,
+		"network":  networks,
 		"logging":  logging,
 	})
 }
@@ -116,6 +177,66 @@ func (m Service) AsComposeServiceConfig() composetypes.ServiceConfig {
 				iTag = i.Tag.ValueString()
 			}
 			service.Image = fmt.Sprintf("%s/%s:%s", iRepo, iName, iTag)
+		}
+	}
+
+	if !m.Ports.IsNull() && !m.Ports.IsUnknown() {
+		ports := []Port{}
+		if diag := m.Ports.ElementsAs(context.Background(), &ports, true); !diag.HasError() {
+			service.Ports = []composetypes.ServicePortConfig{}
+			for _, p := range ports {
+				port := composetypes.ServicePortConfig{
+					Name:        p.Name.ValueString(),
+					Target:      uint32(p.Target.ValueInt64()),
+					Published:   p.Published.ValueString(),
+					Protocol:    p.Protocol.ValueString(),
+					AppProtocol: p.AppProtocol.ValueString(),
+					Mode:        p.Mode.ValueString(),
+					HostIP:      p.HostIP.ValueString(),
+				}
+				service.Ports = append(service.Ports, port)
+			}
+		}
+	}
+
+	if !m.Networks.IsNull() && !m.Networks.IsUnknown() {
+		networks := []ServiceNetwork{}
+		if diag := m.Networks.ElementsAs(context.Background(), &networks, true); !diag.HasError() {
+			service.Networks = map[string]*composetypes.ServiceNetworkConfig{}
+			for _, n := range networks {
+				network := composetypes.ServiceNetworkConfig{
+					Aliases:      []string{},
+					Ipv4Address:  n.Ipv4Address.ValueString(),
+					Ipv6Address:  n.Ipv6Address.ValueString(),
+					LinkLocalIPs: []string{},
+					MacAddress:   n.MacAddress.ValueString(),
+					DriverOpts:   map[string]string{},
+					Priority:     int(n.Priority.ValueInt64()),
+				}
+
+				if n.Aliases.IsNull() || n.Aliases.IsUnknown() {
+					aliases := []string{}
+					if diag := n.Aliases.ElementsAs(context.Background(), &aliases, true); !diag.HasError() {
+						network.Aliases = aliases
+					}
+				}
+
+				if n.LinkLocalIPs.IsNull() || n.LinkLocalIPs.IsUnknown() {
+					linkLocalIPs := []string{}
+					if diag := n.LinkLocalIPs.ElementsAs(context.Background(), &linkLocalIPs, true); !diag.HasError() {
+						network.LinkLocalIPs = linkLocalIPs
+					}
+				}
+
+				if !n.DriverOpts.IsNull() && !n.DriverOpts.IsUnknown() {
+					driverOpts := map[string]string{}
+					if diag := n.DriverOpts.ElementsAs(context.Background(), &driverOpts, true); !diag.HasError() {
+						network.DriverOpts = driverOpts
+					}
+				}
+
+				service.Networks[n.Name.ValueString()] = &network
+			}
 		}
 	}
 
